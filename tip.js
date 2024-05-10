a094cfb1e97acce1c254e12a2a17102e21582551
MaheshKummari <maheshkummari7111@gmail.com>
2024-05-10 17:03:13 +0530

    files updated

--- tip.js
+++ tip.js
@@ -1,33 +1,37 @@
-let calculateBtn = document.getElementById('calculate');
+let calculateBtn = document.getElementById("calculate");
 let resetBtn = document.getElementsByClassName("reset-btn")[0];
-let showTip = document.getElementById("showTip")
+let showTip = document.getElementById("showTip");
+let showTotalBill = document.getElementById("showTotalBill");
 
-calculateBtn.addEventListener("click", function(event){
-    event.preventDefault() // this is preventing "fill empty input" prompt
-    let billAmount = document.getElementById('bill').value ;
-    let tipPercent = document.getElementById("service").value;
-    let people = document.getElementById("people").value;
+calculateBtn.addEventListener("click", function (event) {
+  event.preventDefault(); // this is preventing "fill empty input" prompt
+  let billAmount = Number(document.getElementById("bill").value);
+  let tipPercent = Number(document.getElementById("service").value);
+  let people = Number(document.getElementById("people").value);
 
-    // console.log(`bill amount: ${billAmount},tip: ${tipPercent}, people: ${people}`)
+  // console.log(`bill amount: ${billAmount},tip: ${tipPercent}, people: ${people}`)
 
-    if(billAmount == '' || people == ''){
-        return alert("Please give a valid nubers...")
-    }
-    let totalTip = (Number(tipPercent) / 100 ) * Number(billAmount)
-    // console.log(`total tip : ${totalTip}`)
+  if (billAmount == "" || people == "") {
+    return alert("Please give a valid numbers...");
+  }
+  let totalTip = (tipPercent / 100) * billAmount;
+  // console.log(`total tip : ${totalTip}`)
 
-    let tipPerEach = Math.ceil(totalTip / Number(people))
-    // console.log(`tip foe each : ${tipPerEach}`)
-    // console.log(totalTip / Number(people))
+  let tipPerEach = Math.ceil(totalTip / people);
+  // console.log(`tip foe each : ${tipPerEach}`)
+  // console.log(totalTip / Number(people))
+  let billPerEach = Math.ceil((totalTip + billAmount) / people);
+  //   console.log(billPerEach);
 
-    let message = `Tip amount for Each person is ₹ ${tipPerEach}/-`
-    
-    showTip.textContent = message;
+  let tipMessage = `Tip amount for Each person is ₹ ${tipPerEach}/-`;
+  let totalBillMessage = `Bill amount including tip for Each person is ₹ ${billPerEach}/-`;
 
-
+  showTip.textContent = tipMessage;
+  showTotalBill.textContent = totalBillMessage;
 });
 
-resetBtn.addEventListener("click", function(){
-    showTip.textContent = '';
-    // console.log("hii")
+resetBtn.addEventListener("click", function () {
+  showTip.textContent = "";
+  showTotalBill.textContent = "";
+  // console.log("hii")
 });
